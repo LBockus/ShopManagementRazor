c10c3e49c41d27afffd1516ebc5629c54ff577c0
Lukas Bočkus <93864693+LBockus@users.noreply.github.com>
2022-05-16 20:00:34 +0300

    created mdoel

---
+++ ShopManagement/ShopManagement/Models/ShopItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManagement.Models
+{
+    public class ShopItem
+    {
+        public string name { get; set; }
+        public string shopName { get; set; }
+        public DateTime expiryDate { get; set; }
+    }
+}
