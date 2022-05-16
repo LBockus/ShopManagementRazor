84fbed2c23daeac1f1d86c7608f77e6075c53f2b
Lukas Bočkus <93864693+LBockus@users.noreply.github.com>
2022-05-16 20:24:02 +0300

    Created ShopManagement

---
+++ ShopManagement/ShopManagement/Controllers/ShopController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopManagement.Models;
+using ShopManagement.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManagement.Controllers
+{
+    public class ShopController : Controller
+    {
+        private ShopService _shopService;
+
+        public ShopController(ShopService shopService)
+        {
+            _shopService = shopService;
+        }
+
+        public IActionResult Index()
+        {
+            var shopitem = _shopService.GetAll();
+            return View(shopitem);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ShopItem shopitem = new ShopItem();
+            return View(shopitem);
+        }
+
+        [HttpPost]
+        public IActionResult Add(ShopItem shopitem)
+        {
+            _shopService.Add(shopitem);
+                return RedirectToAction("Index");
+        }
+
+        public IActionResult Remove(string name)
+        {
+            _shopService.Remove(name);
+            return RedirectToAction("Index");
+        }
+    }
+}
