1147df72b6ffc35394c8ae2529a1c8f1c427ea80
Lukas Bočkus <93864693+LBockus@users.noreply.github.com>
2022-05-16 20:14:43 +0300

    created ShopService

---
+++ ShopManagement/ShopManagement/Services/ShopService.cs
@@ -0,0 +1,34 @@
+﻿using ShopManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManagement.Services
+{
+    public class ShopService
+    {
+        public ShopService()
+        {
+
+        }
+
+        private List<ShopItem> _shopItem = new List<ShopItem>();
+
+        public List<ShopItem> GetAll()
+        {
+            return _shopItem;
+        }
+
+        public void Add(ShopItem shopitem)
+        {
+            _shopItem.Add(shopitem);
+        }
+
+        public void Remove(string name)
+        {
+            _shopItem = _shopItem.Where(si => si.name != name).ToList();
+        }
+    }
+}
