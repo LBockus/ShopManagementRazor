40a393f7f81618aac3ee7fe18a6af2e691afd05a
Lukas Bočkus <93864693+LBockus@users.noreply.github.com>
2022-05-16 21:03:01 +0300

    Fixed naming

--- ShopManagement/ShopManagement/Startup.cs
+++ ShopManagement/ShopManagement/Startup.cs
@@ -26,6 +26,7 @@ namespace ShopManagement
         {
             services.AddControllersWithViews();
             services.AddSingleton<ShopService>();
+            services.AddTransient<ShopService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
